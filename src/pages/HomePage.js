5b893dbae7d0d626e5a0033edb0b23528d0d119a
Lee_woo <dlguddn3214@naver.com>
2020-06-30 08:20:36 +0900

    리덕스 사가를 이용한 store값 가져오기

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { plusAction } from "../reducer/test";
 
 const HomePage = () => {
-    return <div>홈</div>;
+    const reduxState = useSelector((state) => state);
+    const dispatch = useDispatch();
+    console.log(reduxState);
+    useEffect(() => {
+        dispatch(plusAction());
+    }, [dispatch]);
+    return <div>홈{reduxState}</div>;
 };
 
 export default HomePage;
