29db18dd167c80d4f9d8a24c8a7f296ddeda2ada
yoos <cgv3737@hodi.com>
2020-06-30 11:11:02 +0900

    React-Leaflet 에러 해결
    Css 적용하여 에러 해결
    Marker Icon도 경로 지정하여 표시하게끔 변경

--- src/components/molecules/ReactLeaflet.js
+++ src/components/molecules/ReactLeaflet.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Leaflet from 'leaflet';
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 
 const ReactLeaflet = () => {
     const position = [51.505, -0.09];
     const map = (
-        <Map center={position} zoom={13}>
+        <Map center={position} zoom={13} style={{height: '400px'}}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -25,5 +27,16 @@ const ReactLeaflet = () => {
         </div>
     );
 };
+Leaflet.Icon.Default.imagePath =
+    '../node_modules/leaflet'
+
+delete Leaflet.Icon.Default.prototype._getIconUrl;
+
+Leaflet.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 export default ReactLeaflet;
+
