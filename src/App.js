358d32afb9f15aac57d5ed519e80124702d41e35
Lee_woo <dlguddn3214@naver.com>
2020-07-09 08:53:03 +0900

    App.js 라우터 정리

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import UserJoinPage from "./pages/UserJoinPage";
-import UserLoginPage from "./pages/UserLoginPage";
-import UserMyPage from "./pages/UserMyPage";
-import RoomHostPage from "./pages/RoomHostPage";
-import RoomListPage from "./pages/RoomListPage";
-import RoomDetailPage from "./pages/RoomDetailPage";
+import HomePage from "./components/pages/HomePage";
+import UserJoinPage from "./components/pages/UserJoinPage";
+import UserLoginPage from "./components/pages/UserLoginPage";
+import UserMyPage from "./components/pages/UserMyPage";
+import RoomHostPage from "./components/pages/RoomHostPage";
+import RoomListPage from "./components/pages/RoomListPage";
+import RoomDetailPage from "./components/pages/RoomDetailPage";
 import ReactLeaflet from "./components/molecules/ReactLeaflet";
-import GNBComponent from "./components/molecules/GNBComponent";
 
 function App() {
     return (
         <BrowserRouter>
-            <GNBComponent></GNBComponent>
             <Switch>
-                <Route path={"/"} exact>
-                    <HomePage></HomePage>
-                </Route>
+                <Route path={"/"} exact component={HomePage} />
                 <Route path={"/user/join"} component={UserJoinPage} />
                 <Route path={"/user/login"} component={UserLoginPage} />
                 <Route path={"/user/mypage"} component={UserMyPage} />
                 <Route path={"/room/host"} component={RoomHostPage} />
-
-                <Route path={"/room/list"} exact>
-                    <RoomListPage></RoomListPage>
-                </Route>
+                <Route path={"/room/list"} component={RoomListPage}></Route>
                 <Route path={"/room/detail/:id?"} component={RoomDetailPage} />
-                <Route path={"/test"}>
-                    <ReactLeaflet
-                        lat="37.5666805"
-                        lng=" 126.9784147"
-                    ></ReactLeaflet>
-                </Route>
+                <Route
+                    path={"/test"}
+                    render={() => (
+                        <ReactLeaflet lat="37.5666805" lng=" 126.9784147" />
+                    )}
+                />
                 <Route
                     path="*"
                     render={() => {
