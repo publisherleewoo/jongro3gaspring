eeefd28f7337215f6f78179047a1a73621f30603
Lee_woo <dlguddn3214@naver.com>
2020-07-12 03:20:48 +0900

    Updates

--- precache-manifest.3b5cd1e524f6fd3f93cf0f68a476e5bc.js
+++ precache-manifest.3b5cd1e524f6fd3f93cf0f68a476e5bc.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8eb14a78d440c97bc43075f1908d7017",
+    "revision": "6af0d073410f593d9b8b1b932546ca2f",
     "url": "/jongro3gaspring/index.html"
   },
   {
-    "revision": "2a9ee7017dfcb4a7135a",
+    "revision": "84dddf804167674c1273",
     "url": "/jongro3gaspring/static/css/2.b7cc38ad.chunk.css"
   },
   {
-    "revision": "a9dd6396479bbd2ae502",
+    "revision": "c84297c79b45a975353d",
     "url": "/jongro3gaspring/static/css/main.8a43f95b.chunk.css"
   },
   {
-    "revision": "2a9ee7017dfcb4a7135a",
-    "url": "/jongro3gaspring/static/js/2.12a16c39.chunk.js"
+    "revision": "84dddf804167674c1273",
+    "url": "/jongro3gaspring/static/js/2.066c9344.chunk.js"
   },
   {
     "revision": "6791e7fb9584d65ed87d655035c7da95",
-    "url": "/jongro3gaspring/static/js/2.12a16c39.chunk.js.LICENSE.txt"
+    "url": "/jongro3gaspring/static/js/2.066c9344.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a9dd6396479bbd2ae502",
-    "url": "/jongro3gaspring/static/js/main.46ebf24a.chunk.js"
+    "revision": "c84297c79b45a975353d",
+    "url": "/jongro3gaspring/static/js/main.22410b27.chunk.js"
   },
   {
     "revision": "b7577b4afbf53f5ce8a0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jongro3gaspring/precache-manifest.32dee920d2ae191ac2e2e13ae16b1aef.js"
+  "/jongro3gaspring/precache-manifest.3b5cd1e524f6fd3f93cf0f68a476e5bc.js"
 );
 
 self.addEventListener('message', (event) => {
